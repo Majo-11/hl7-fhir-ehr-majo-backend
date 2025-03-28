1b18460a55f8e9d55ae6cb4b836cc749c161ce03
Majo-11 <mariajo.pineda@urosario.edu.co>
2025-03-28 16:55:18 -0500

    Cambio 1

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Request
 import uvicorn
-from app.controlador.PatientCrud import GetPatientById,WritePatient
+from app.controlador.PatientCrud import GetPatientById,WritePatient,GetPatientByIdentifier
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -20,6 +20,17 @@ async def get_patient_by_id(patient_id: str):
         return patient  # Return patient
     elif status=='notFound':
         raise HTTPException(status_code=404, detail="Patient not found")
+    else:
+        raise HTTPException(status_code=500, detail=f"Internal error. {status}")
+
+@app.get("/patient", response_model=dict)
+async def get_patient_by_id(system: str, value: str):
+    print("solicitud datos",system,value)
+    status,patient = GetPatientByIdentifier(system,value)
+    if status=='success':
+        return patient  # Return patient
+    elif status=='notFound':
+        raise HTTPException(status_code=204, detail="Patient not found")
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
